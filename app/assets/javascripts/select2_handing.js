9869f7294a527725fdb7690da68994ac17b15c20
stevieing <stevieing@sky.com>
2017-08-07 15:46:41 +0100

    Dam you turbolinks.

--- app/assets/javascripts/select2_handing.js
+++ app/assets/javascripts/select2_handing.js
@@ -1,7 +1,8 @@
 // Select 2 is a javascript library to provide advanced
 // select dropdowns, including typing.
-$(function() {
+
+$(document).on('turbolinks:load', function() {
   $(".select2").select2({
     theme: "bootstrap"
   });
-})
+});
