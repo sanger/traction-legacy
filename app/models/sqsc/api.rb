32543d900d6350568d2a0db9f971521e94c551a4
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-08-08 09:11:38 +0100

    Minor changes

--- app/models/sqsc/api.rb
+++ app/models/sqsc/api.rb
@@ -3,12 +3,11 @@
 require 'json_api_client'
 
 module Sqsc
-  # for classes/objects created from sqsc responces using json api client
+  # for classes/objects created from sqsc responses using json api client
+  # initializer is in config/initializers/sqsc_api.rb
   module Api
     # "abstract" base class
     class Base < JsonApiClient::Resource
-      # should go to configuration SqscApi::Base.site = ''
-      self.site = 'http://localhost:3000/api/v2/'
     end
 
     class Sample < Base
