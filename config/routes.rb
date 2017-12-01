e1cfa0083351180ed2c881a42503dcf97ce52af8
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2017-12-01 14:50:43 +0000

    Changed pipeline URL route to use name instead of id

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pipelines, only: [:index] do
+  resources :pipelines, param: :name, only: [:index] do
     resources :work_orders, only: %i[index show] do
       resources :lab_events
     end
